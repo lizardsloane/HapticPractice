7d3e3151f99346a2762a68f797b386c91e01e8e6
lizardsloane <118654911+lizardsloane@users.noreply.github.com>
2023-05-02 20:14:25 -0700

    Add files via upload

---
+++ HapticPractice/HapticPractice/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  HapticPractice
+//
+//  Created by Leah Cluff on 5/2/23.
+//
+
+import SwiftUI
+import CoreHaptics
+
+
+struct ContentView: View {
+    
+    @State private var engine: CHHapticEngine?
+    
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            
+            Text("Trigger Haptics")
+                .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+            .foregroundColor(Color.white)
+            .background(Color.green)
+            .cornerRadius(3)
+            .cornerRadius(4)
+            
+            
+          
+        }
+        
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HapticPractice/HapticPractice/HapticPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticPracticeApp.swift
+//  HapticPractice
+//
+//  Created by Leah Cluff on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HapticPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
